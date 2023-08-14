c6553c48102548f9c706fb0dac6d8425a83d2872
Iamneo <techadmin@iamneo.ai>
2023-08-14 17:17:35 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -36,17 +36,20 @@ function App() {
         <div>
             <div className="flex items-center justify-center space-x-4 mb-12">
               <h1 className="text-3xl">Quizz App</h1>
-<i className="">learn react</i>
-</div>
-{/* {start && <Button onClick={ handleStart} style={"bg-orange-
+              <i className="">learn react</i>
+            </div>
+                {/* {start && <Button onClick={ handleStart} style={"bg-orange-
 300 px-10 py-2 rounded hover:bg-orange-400"}>Start Quiz</Button>	} */}
-{banner && <Banner score={attempt} />}
-{card && <div> {questions.map((q, index) => (
-<Card key={index} question={q.question} correctAnswerMarkUpda
-te={correctAnswerMarkUpdate} attempt={attempt} options={opt} answer={q.answer} setQsns={setqsnAttempt} qsn={ qsnAttempt} setCard={setResult} qsnAttempt={qsn Attempt} />
-))} </div>}
-{result && <Button onClick={handleResult} style={"bg-orange-300 px-
-10 py-2 rounded hover:bg-orange-400 mt-3"} >Show Results</Button>}
-{start && <Button onClick={ handleStart} style={"bg-orange-300 px10 py-2 rounded hover:bg-orange-400"}>Start Quiz</Button>	} </div>
-</div>
-); } export default App;
+                {banner && <Banner score={attempt} />}
+            {card && <div> {questions.map((q, index) => (
+                <Card key={index} question={q.question} correctAnswerMarkUpda
+te={correctAnswerMarkUpdate} attempt={attempt} options={opt} answer={q.answer} setQsns={setqsnAttempt} qsn={ qsnAttempt} setCard={setResult} qsnAttempt={qsnAttempt} />
+              ))} 
+            </div>}
+            {result && <Button onClick={handleResult} style={"bg-orange-300 px-10 py-2 rounded hover:bg-orange-400 mt-3"} >Show Results</Button>}
+            {start && <Button onClick={ handleStart} style={"bg-orange-300 px-10 py-2 rounded hover:bg-orange-400"}>Start Quiz</Button>	} 
+        </div>
+      </div>
+    ); 
+} 
+export default App;
